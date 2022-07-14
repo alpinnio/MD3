40abfa8dc3d9a0e894bdd3f3de164c5a3ff65ea3
alpinnio <107537249+alpinnio@users.noreply.github.com>
2022-07-14 18:19:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6285748894171'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['6288210072767'] // Put your number here
+global.mods = [6288210072756] // Want some help?
+global.prems = [6288210072756] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
@@ -43,11 +43,11 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'stickerby'
-global.author = `NakataArdnt`
-global.wm = 'NahBotz-' //Nama Bot
-global.watermark = wm
+global.author = `LuxxyXyz`
+global.wm = 'LuccyBotz-' //Nama Bot
+global.watermark = wm luxxy
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/nakataa1.a'
+global.instagram = 'https://instagram.com/luxxyxyz'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
